6715b019aeca86720aa3d77c1324dbe1ec9946ac
Erica Stith <rexlunae@gmail.com>
2023-03-10 17:54:04 -0800

    Added parameter parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    output: Option<std::path::PathBuf>,
+
+    #[arg(last = true)]
+    inputs: Vec<std::path::PathBuf>,
+}
+
 fn main() {
+    let args = Args::parse();
+
     println!("Hello, world!");
 }
