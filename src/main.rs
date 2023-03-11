ca6fcc33ad2081d49140c71771a5b85c7b7a2986
Erica Stith <rexlunae@gmail.com>
2023-03-10 19:45:38 -0800

    Added compilation.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
+use std::path::{Path, PathBuf};
+use std::fs::read_to_string;
+
 use clap::Parser;
+use python_ast::{parse, PythonContext, CodeGen};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    output: Option<std::path::PathBuf>,
+    #[arg(short, long, help="The output file.")]
+    output: Option<PathBuf>,
+    #[arg()]
+    inputs: Vec<PathBuf>,
 
-    #[arg(last = true)]
-    inputs: Vec<std::path::PathBuf>,
+    #[clap(long, short, action, help="Don't actually compile, just output the ast.")]
+    ast_only: bool,
 }
 
 fn main() {
     let args = Args::parse();
+    let mut ctx = PythonContext::default();
 
-    println!("Hello, world!");
+    for input in args.inputs {
+        let py = read_to_string(input).unwrap();
+        let ast = parse(&py, "__main__").unwrap();
+
+        let output = if args.ast_only {
+            format!("{:?}", ast)
+        } else {
+            format!("{}", ast.to_rust(&mut ctx).unwrap())
+        };
+        println!("{}", output);
+    }
+
 }
---
+++ test/test.py
@@ -0,0 +1,4 @@
+def foo():
+    print("I am testing this.")
+
+foo()
