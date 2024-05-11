e357088e1d06bf72d0516494e395c09c12f35e4c
Erica Stith <rexlunae@gmail.com>
2024-05-10 22:47:26 -0700

    cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::path::PathBuf;
 use std::fs::{read_to_string, File};
 use std::io::prelude::*;
+use std::path::PathBuf;
 use std::time::SystemTime;
 
 use clap::Parser;
-use python_ast::{parse, PythonOptions, CodeGen, CodeGenContext, symbols::SymbolTableScopes};
+use python_ast::{parse, symbols::SymbolTableScopes, CodeGen, CodeGenContext, PythonOptions};
 use rust_format::{Formatter, RustFmt};
 
-use anyhow::{Result};
+use anyhow::Result;
 
 // Set up the fern logging facility.
-fn setup_logger(level: log::LevelFilter, log_file: Option<String>) -> std::result::Result<(), fern::InitError> {
+fn setup_logger(
+    level: log::LevelFilter,
+    log_file: Option<String>,
+) -> std::result::Result<(), fern::InitError> {
     let mut logger = fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
@@ -26,7 +29,7 @@ fn setup_logger(level: log::LevelFilter, log_file: Option<String>) -> std::resul
         .chain(std::io::stdout());
 
     match log_file {
-        None => {},
+        None => {}
         Some(s) => logger = logger.chain(fern::log_file(s)?),
     };
     logger.apply()?;
@@ -36,27 +39,37 @@ fn setup_logger(level: log::LevelFilter, log_file: Option<String>) -> std::resul
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, help="The output file.")]
+    #[arg(short, long, help = "The output file.")]
     output: Option<PathBuf>,
     #[arg()]
     inputs: Vec<PathBuf>,
 
-    #[clap(long, short, action, help="Nicely format the output.")]
+    #[clap(long, short, action, help = "Nicely format the output.")]
     pretty: bool,
 
-    #[clap(long, short, action, help="Don't actually compile, just output the ast represented as a Rust data structure.")]
+    #[clap(
+        long,
+        short,
+        action,
+        help = "Don't actually compile, just output the ast represented as a Rust data structure."
+    )]
     ast_only: bool,
 
-    #[clap(long, short, action, help="Don't actually compile, just output the symbols.")]
+    #[clap(
+        long,
+        short,
+        action,
+        help = "Don't actually compile, just output the symbols."
+    )]
     symbols_only: bool,
 
     #[clap(long, short, action, help="Sets the log level. Values are: off,error,warn,info,debug,trace", default_value_t=log::LevelFilter::Warn)]
     log_level: log::LevelFilter,
 
-    #[clap(long, action, help="Write log events to this file.")]
+    #[clap(long, action, help = "Write log events to this file.")]
     log_file: Option<String>,
 
-    #[clap(long, short, action, help="Compile without stdpython.")]
+    #[clap(long, short, action, help = "Compile without stdpython.")]
     nostd: bool,
 }
 
@@ -89,7 +102,12 @@ fn main() -> Result<()> {
                     format!("{:?}", symbols)
                 }
             } else {
-                let rust = ast.to_rust(CodeGenContext::Module(module_name), options.clone(), symbols.clone())
+                let rust = ast
+                    .to_rust(
+                        CodeGenContext::Module(module_name),
+                        options.clone(),
+                        symbols.clone(),
+                    )
                     .expect("converting to Rust");
                 if args.pretty {
                     let unformated = rust.to_string();
@@ -110,7 +128,6 @@ fn main() -> Result<()> {
             file.write_all(file_output.as_bytes())?;
         }
         None => println!("{}", file_output),
-
     }
 
     Ok(())
